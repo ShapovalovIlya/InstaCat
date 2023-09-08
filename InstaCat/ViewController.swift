c4247126acb47d029ec00dd7d6cbf7f0a57a9298
Ilia Shapovalov <greateman@mail.ru>
2023-09-09 01:19:11 +0300

    Initial Commit

---
+++ InstaCat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  InstaCat
+//
+//  Created by Илья Шаповалов on 09.09.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
